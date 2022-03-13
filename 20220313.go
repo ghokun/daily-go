9567803fb39ecb43f1698c69752435e07c61e6d2
ghokun <gokhun@gmail.com>
2022-03-13 21:58:04 +0300

    Add 20220313
    Signed-off-by: ghokun <gokhun@gmail.com>

---
+++ 20220313.go
@@ -0,0 +1,29 @@
+package dailygo
+
+/*
+Given an array of integers, return a new array such that each element at index i
+of the new array is the product of all the numbers in the original array except the one at i.
+
+For example, if our input was [1, 2, 3, 4, 5], the expected output would be
+[120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+Follow-up: what if you can't use division?
+*/
+func _202203123(numbers []int) []int {
+	length := len(numbers)
+	var forward = make([]int, length)
+	var backward = make([]int, length)
+	var product = make([]int, length)
+
+	forward[0] = 1
+	backward[length-1] = 1
+
+	for i := 1; i < length; i++ {
+		forward[i] = forward[i-1] * numbers[i-1]
+		backward[length-i-1] = backward[length-i] * numbers[length-i]
+	}
+	for i := 0; i < length; i++ {
+		product[i] = forward[i] * backward[i]
+	}
+	return product
+}
---
+++ 20220313_test.go
@@ -0,0 +1,60 @@
+package dailygo
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_202203123(t *testing.T) {
+	type args struct {
+		numbers []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "Test 1",
+			args: args{
+				numbers: []int{1, 2, 3, 4, 5},
+			},
+			want: []int{120, 60, 40, 30, 24},
+		},
+		{
+			name: "Test 2",
+			args: args{
+				numbers: []int{3, 2, 1},
+			},
+			want: []int{2, 3, 6},
+		},
+		{
+			name: "Test 3",
+			args: args{
+				numbers: []int{3, 0, 1},
+			},
+			want: []int{0, 3, 0},
+		},
+		{
+			name: "Test 4",
+			args: args{
+				numbers: []int{2, 7, 4, 3, 1},
+			},
+			want: []int{84, 24, 42, 56, 168},
+		},
+		{
+			name: "Test 5",
+			args: args{
+				numbers: []int{2, 7, 4, 3, 0},
+			},
+			want: []int{0, 0, 0, 0, 168},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := _202203123(tt.args.numbers); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("_202203123() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
