cc1bec6a7a5d64c26d58143f8c62be87eacd6f66
ghokun <gokhun@gmail.com>
2022-03-21 01:47:29 +0300

    Add 20220314
    Signed-off-by: ghokun <gokhun@gmail.com>

--- 20220314.go
+++ 20220314.go
@@ -1,3 +1,44 @@
 package dailygo
 
-func _20220314() {}
+/*
+Given an array of integers, find the first missing positive integer in linear time and constant space. In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicates and negative numbers as well.
+
+For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+
+You can modify the input array in-place.
+*/
+
+func _20220314(arr []int) int {
+	moveToValuePosition := func(arr []int, index int, value int) int {
+		temp := arr[value-1]
+		arr[value-1] = arr[index]
+		arr[index] = temp
+		return temp
+	}
+
+	length := len(arr)
+	for index, value := range arr {
+		// first pass
+		if value <= 0 || value > length {
+			continue
+		}
+		if arr[value-1] == value {
+			continue
+		}
+		temp := moveToValuePosition(arr, index, value)
+		// first pass
+		if temp <= 0 || temp > length {
+			continue
+		}
+		if arr[temp-1] == temp {
+			continue
+		}
+		moveToValuePosition(arr, index, temp)
+	}
+	for index, value := range arr {
+		if value != index+1 {
+			return index + 1
+		}
+	}
+	return length + 1
+}
---
+++ 20220314_test.go
@@ -0,0 +1,57 @@
+package dailygo
+
+import "testing"
+
+func Test_20220314(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Test 1",
+			args: args{
+				arr: []int{3, 4, 2, 1},
+			},
+			want: 5,
+		},
+		{
+			name: "Test 2",
+			args: args{
+				arr: []int{3, 4, -1, 1},
+			},
+			want: 2,
+		},
+		{
+			name: "Test 3",
+			args: args{
+				arr: []int{1, 2, 0, 1},
+			},
+			want: 3,
+		},
+		{
+			name: "Test 4",
+			args: args{
+				arr: []int{-1, -2, 0, -1},
+			},
+			want: 1,
+		},
+		{
+			name: "Test 5",
+			args: args{
+				arr: []int{1,-1,-5,-3,3,4,2,8},
+			},
+			want: 5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := _20220314(tt.args.arr); got != tt.want {
+				t.Errorf("_20220314() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
